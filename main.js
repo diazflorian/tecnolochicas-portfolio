eb1102d6fcf85101c91af8d0c628c5620c6f4675
Yohanna Diaz Florian <diazfy1909@gmail.com>
2023-12-09 19:57:14 -0400

    Update main.js in main

--- main.js
+++ main.js
@@ -3,7 +3,6 @@ let app = document.getElementById('typewriter');
 let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
- cursor: "<span style='color: #fb4993;'>|</span>",
 });
  
 typewriter
